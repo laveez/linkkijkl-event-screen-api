c9829084e88eb8675ecad539e95660a29fcf5a39
Laveez <niko.muukkonen@gofore.com>
2024-05-16 12:38:44 +0300

    Add a keep alive API and API call to keep the deployment from spinning down

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const eventsRoutes = require('./events');
 const lunchRoutes = require('./lunch');
 const sponsorsRoutes = require('./sponsors');
+const keepAliveRoutes = require('./keepAlive');
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(cors());
 app.use('/events', eventsRoutes);
 app.use('/lunch', lunchRoutes);
 app.use('/sponsors', sponsorsRoutes);
+app.use('/keep-alive', keepAliveRoutes);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
